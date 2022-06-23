2d767e2b0e2a3670df00cd024a7b20eabe318bb2
Niklaus Maurer <nick.is.normal@gmail.com>
2022-06-23 12:42:19 +0200

    adds script to compare conversion rates

---
+++ compare-conversion-rates.r
@@ -0,0 +1,10 @@
+
+n_a <- 80000 # size of sample A
+c_a <- 1600  # conversions within sample A
+n_b <- 80000 # size of sample B
+c_b <- 1696  # conversions within sample B
+
+contingencytab <- matrix(c(n_a - c_a, n_b - c_b, c_a, c_b), ncol = 2, nrow = 2)
+print(contingencytab)
+
+print(fisher.test(contingencytab, alternative = "greater"))
